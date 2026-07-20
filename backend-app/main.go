ba78073a05cadcf204e2c845ee443bb30212f9bc
Neavtixs <sandincl.d@gmail.com>
2026-07-20 22:05:25 +0800

    add endpoint get presign url s3

--- backend-app/main.go
+++ backend-app/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"net/http"
 	"nonton/backend-app/configs"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := configs.NewGin()
-	aws := configs.NewS3()
+	storage := configs.NewS3()
 
+	// check health
 	server.GET("/api/s3/check", func(ctx *gin.Context) {
-		err := aws.HealthCheck(ctx)
+		err := storage.HealthCheck(ctx)
 		if err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{
 				"sucess": false,
@@ -22,6 +26,29 @@ func main() {
 
 		ctx.JSON(http.StatusOK, gin.H{
 			"sucess": true,
+		})
+	})
+
+	// get presigned url s3
+	server.GET("/api/upload", func(ctx *gin.Context) {
+		result, err := storage.PresignClient.PresignPutObject(ctx, &s3.PutObjectInput{
+			Bucket:      aws.String(storage.Bucket),
+			Key:         aws.String("baru.txt"),
+			ContentType: aws.String("txt"),
+		}, func(po *s3.PresignOptions) {
+			po.Expires = 15 * time.Minute
+		})
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"success": false,
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"data":    result.URL,
 		})
 	})
 
