12424f19e00253048bb3465f3907413b4f1297eb
Neavtixs <sandincl.d@gmail.com>
2026-07-20 21:44:47 +0800

    api check heatlh s3

--- backend-app/main.go
+++ backend-app/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"nonton/backend-app/configs"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := configs.NewGin()
+	aws := configs.NewS3()
+
+	server.GET("/api/s3/check", func(ctx *gin.Context) {
+		err := aws.HealthCheck(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"sucess": false,
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"sucess": true,
+		})
+	})
+
+	server.Run()
 }
