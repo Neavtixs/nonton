83cf38ab2e56d92fab048a8cd8f3e2d231bb6d6d
Neavtixs <sandincl.d@gmail.com>
2026-07-20 23:30:30 +0800

    use for loop for every file upload

--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -1,12 +1,55 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 const ApiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
-  function getPresignUrl() {}
+  const [file, setFile] = useState<FileList | null>(null);
+
+  const getPresignUrl = async () => {
+    console.table(file);
+
+    if (file) {
+      for (var i = 0; i < file.length; i++) {
+        const data = await fetch(`${ApiUrl}/api/upload`, {
+          method: "POST",
+          body: JSON.stringify({
+            filename: file[i].webkitRelativePath,
+            content_type: file[i].type,
+          }),
+        })
+          .then((resp) => resp.json())
+          .then((res) => res.data);
+
+        await fetch(data, {
+          method: "PUT",
+          headers: {
+            "Content-Type": file[i].type,
+          },
+          body: file[i],
+        }).then((resp) => console.log(i, resp.ok));
+      }
+    }
+  };
+
   return (
     <div className="p-3">
-      <h1>anjay</h1>
+      <div>
+        <Field>
+          <FieldLabel>Select Movie</FieldLabel>
+          <Input
+            type="file"
+            //multiple
+            {...({ webkitdirectory: "", directory: "" } as any)}
+            onChange={(e) => setFile(e.target.files)}
+          />
+        </Field>
+      </div>
+      <Button onClick={getPresignUrl}>GET URL</Button>
     </div>
   );
 }
