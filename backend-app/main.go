58aa05f94f7043158c7373f62499bc919564ac3e
Neavtixs <sandincl.d@gmail.com>
2026-07-21 09:06:11 +0800

    update api get presign can 100 batch

--- backend-app/main.go
+++ backend-app/main.go
@@ -33,39 +33,63 @@ func main() {
 		})
 	})
 
-	// get presigned url s3
-	type PresignRequest struct {
+	// get presigned url s3, to upload batch of hls
+	type FileRequest struct {
 		Filename    string `json:"filename"`
 		ContentType string `json:"content_type"`
+	}
+
+	type PresignRequest struct {
+		Files []FileRequest `json:"files"`
+	}
+
+	type PresignResponse struct {
+		Key       string `json:"key"`
+		UploadURL string `json:"upload_url"`
 	}
 	server.POST("/api/upload", func(ctx *gin.Context) {
 		req := PresignRequest{}
 		ctx.ShouldBindJSON(&req)
 		fmt.Println(req)
 
-		result, err := storage.PresignClient.PresignPutObject(ctx, &s3.PutObjectInput{
-			Bucket:      aws.String(storage.Bucket),
-			Key:         aws.String(req.Filename),
-			ContentType: aws.String(req.ContentType),
-		}, func(po *s3.PresignOptions) {
-			po.Expires = 15 * time.Minute
-		})
+		var response []PresignResponse
 
-		if err != nil {
+		if len(req.Files) > 100 {
 			ctx.JSON(http.StatusBadRequest, gin.H{
 				"success": false,
+				"msg":     "to much file",
 			})
 			return
+		}
+		for _, file := range req.Files {
+			result, err := storage.PresignClient.PresignPutObject(ctx, &s3.PutObjectInput{
+				Bucket:      aws.String(storage.Bucket),
+				Key:         aws.String(file.Filename),
+				ContentType: aws.String(file.ContentType),
+			}, func(po *s3.PresignOptions) {
+				po.Expires = 15 * time.Minute
+			})
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"success": false,
+				})
+				return
+			}
+
+			response = append(response, PresignResponse{
+				Key:       file.Filename,
+				UploadURL: result.URL,
+			})
 		}
 
 		ctx.JSON(http.StatusOK, gin.H{
 			"success": true,
-			"data":    result.URL,
+			"data":    response,
 		})
 	})
 
+	// get presign to get file
 	server.GET("/api/file", func(ctx *gin.Context) {
-
 		result, err := storage.PresignClient.PresignGetObject(ctx, &s3.GetObjectInput{
 			Bucket: aws.String(storage.Bucket),
 			Key:    aws.String("citizenofakind/output/master.m3u8"),
